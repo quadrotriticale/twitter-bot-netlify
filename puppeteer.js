242851ca626289e06e576028a4ed5df808e9c362
cytoplasm <elenakah3893@gmail.com>
2023-06-26 18:03:34 +0200

    Changes.

--- puppeteer.js
+++ puppeteer.js
@@ -1,10 +1,11 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 
 const RANDOM_URL = 'https://commons.wikimedia.org/wiki/Special:Random/Image';
 
 
 const getImage = async () => {
-    const browser = await puppeteer.launch({headless: "new"});
+    const browser = await puppeteer.launch({executablePath: await chromium.executablePath});
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
     await page.goto(RANDOM_URL);
